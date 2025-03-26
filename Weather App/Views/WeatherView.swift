96f82413818306297725f5687394dff6cc22e4be
jeevanramakichenin <jeevanramakichenin@gmail.com>
2025-03-26 19:22:08 +0100

    Update

--- Weather App/Views/WeatherView.swift
+++ Weather App/Views/WeatherView.swift
@@ -8,11 +8,92 @@
 import SwiftUI
 
 struct WeatherView: View {
+    var weater: ResponseBody
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weater.name)
+                        .bold().font(.title)
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                VStack {
+                    HStack{
+                        VStack(spacing: 20) {
+                            Image(systemName: "sun.max")
+                                .font(.system(size: 40))
+                            Text(weater.weather[0].main)
+                        }
+                        .frame(width: 150, alignment: .leading )
+                        
+                        Spacer()
+                        
+                        Text(weater.main.feelsLike.roundDouble() + "°" )
+                            .font(.system(size: 100))
+                            .fontWeight(.bold)
+                            .padding()
+                    }
+                    Spacer()
+                        .frame(height: 30)
+                    
+                    AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) { image in
+                                            image
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 350)
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                    Spacer() 
+
+                }
+                
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack{
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Weather now")
+                        .bold().padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer", name: "min temp", value: weater.main.tempMin.roundDouble() + "°")
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "max temp", value: weater.main.tempMax.roundDouble() + "°")
+                    }
+                    
+                    HStack {
+                        WeatherRow(logo: "wind", name: "wind speed", value: weater.wind.speed.roundDouble() + "m/s")
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "humidity", value: weater.main.humidity.roundDouble() + "%")
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .padding(.horizontal, 10)
+                .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
     }
 }
-
+   
 #Preview {
-    WeatherView()
+    WeatherView(weater: previewWeather)
 }
